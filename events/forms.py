091e1edf943ced132cf67fa14facd6d6b5b57206
Rodrigue Roland CODO <codorol22@gmail.com>
2023-01-21 12:45:40 +0500

    Events list display updated

--- events/forms.py
+++ events/forms.py
@@ -16,10 +16,20 @@ class EventForm(forms.ModelForm):
             'calendar', 'summary', 'location', 'description', 'start', 'end'
         ]
         widgets = {
-            'calendar': forms.Select(attrs={'class': 'form-control'}),
-            'summary': forms.TextInput(attrs={'class': 'form-control'}),
-            'location': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control'}),
-            'start': forms.DateTimeInput(attrs={'class': 'form-control'}),
-            'end': forms.DateTimeInput(attrs={'class': 'form-control'}),
+            'calendar':
+            forms.Select(attrs={'class': 'form-control'}),
+            'summary':
+            forms.TextInput(attrs={'class': 'form-control'}),
+            'location':
+            forms.TextInput(attrs={'class': 'form-control'}),
+            'description':
+            forms.Textarea(attrs={
+                'class': 'form-control',
+                'cols': 40,
+                'rows': 4
+            }),
+            'start':
+            forms.DateTimeInput(attrs={'class': 'form-control'}),
+            'end':
+            forms.DateTimeInput(attrs={'class': 'form-control'}),
         }
--- events/models.py
+++ events/models.py
@@ -28,6 +28,14 @@ class Calendar(models.Model):
                                          defaults=defaults)
 
 
+def default_start():
+    return datetime.datetime.utcnow() + datetime.timedelta(days=1)
+
+
+def default_end():
+    return datetime.datetime.utcnow() + datetime.timedelta(days=2)
+
+
 class Event(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
@@ -35,14 +43,35 @@ class Event(models.Model):
     summary = models.CharField(max_length=255)
     location = models.CharField(max_length=255, blank=True)
     description = models.TextField(blank=True)
-    start = models.DateTimeField(default=datetime.datetime.utcnow() +
-                                 datetime.timedelta(days=1))
-    end = models.DateTimeField(default=datetime.datetime.utcnow() +
-                               datetime.timedelta(days=2))
+    start = models.DateTimeField(default=default_start)
+    end = models.DateTimeField(default=default_end)
     attendees = models.ManyToManyField(User, related_name='attendees')
 
     def __str__(self):
         return self.summary
+
+    @property
+    def show_start(self):
+        return self.start.strftime('%Y-%m-%d %H:%M')
+
+    @property
+    def show_end(self):
+        return self.end.strftime('%Y-%m-%d %H:%M')
+
+    @property
+    def show_location(self):
+        if self.location:
+            if len(self.location) > 20:
+                return self.location[:20] + '...'
+        return 'No location'
+
+    @property
+    def show_description(self):
+        if self.description:
+            if len(self.description) > 100:
+                return self.description[:100] + '...'
+            return self.description
+        return 'No description'
 
     @classmethod
     def from_events_list(cls, user, calendar, events):
